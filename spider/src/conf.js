d51bca9e8e6b5c42f27c2cb5f2032120d7eb88bc
lixin <lixinit@gmail.com>
2017-04-28 18:52:47 +0800

    spider interval

--- spider/src/conf.js
+++ spider/src/conf.js
@@ -1,6 +1,6 @@
 const conf = {
-  floorTime: 1000,
-  ceilTime: 3000,
+  floorTime: 10000,
+  ceilTime: 30000,
   ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
 
   contentType: {
